6ff2a11547b4de0a13e7e7dafbc3a488e8ec2cbe
Mateo Garcia <mateog@stanford.edu>
2017-12-13 21:16:58 -0800

    Code is a little jank rn. Trying to figure out how to combine recursive calls with keeping the best solution. Brain is so fried and burnt out.

--- Code/wavy_line_ai/util.py
+++ Code/wavy_line_ai/util.py
@@ -38,17 +38,53 @@ class DepthFirstSearchIterativeDeepening(SearchAlgorithm):
         self.totalCost = None
         self.numStatesExplored = 0
 
+        self.problem = problem
+
         # minimumCostPath = None
 
         # startState = problem.startState()
 
         # Begin recursion
 
-    def recurse(state):
-        # If isEnd(state) and depth == depth
+        while True:
+            recurse()
+            
 
+        self.numStatesExplored, self.depth, self.totalCost, self.actions, \
+            self.endState = recurse()
 
-        # TODO: START HERE
+        if self.verbose >= 1:
+            print "numStatesExplored = %d" % self.numStatesExplored
+            print "depth = %s" % depth
+            print "totalCost = %s" % self.totalCost
+            print "actions = %s" % self.actions
+            print "endState = %s" % self.endState
+
+        self.noPathFound()
+
+    def recurse(self, pastActions, state, pastCost, numStatesExplored, depth):
+        if state is None:
+            self.noPathFound()
+
+        self.numStatesExplored += 1
+        if self.verbose >= 2:
+            print "Exploring %s with pastCost %s" % (state, pastCost)
+        
+        # Check if we've reached an end state; if so, extract solution.
+        if self.problem.isEnd(state) or depth == self.maxDepth:
+            return (numStatesExplored, depth, pastCost, pastActions, state)         # TODO: Start here: Append to a list of solutions
+
+        # Expand from |state| to new successor states,
+        # updating the frontier with each newState.
+        for action, newState, cost in problem.succAndCost(state):
+            if self.verbose >= 3:
+                print "  Action %s => %s with cost %s + %s" % (action, newState, pastCost, cost)
+            actions = list(pastActions) + action
+            return self.recurse(actions, newState, pastCost + cost, depth + 1)
+
+    def noPathFound(self):
+        if self.verbose >= 1:
+            print "No path found"
 
 
 
